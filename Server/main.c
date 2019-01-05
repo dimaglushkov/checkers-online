1eba89c9a7e6b38ffdb07d6ee2a676c44cdd8b39
allacee <allacee>
2019-01-05 22:34:16 +0300

    Now server side works properly

--- Server/main.c
+++ Server/main.c
@@ -1,61 +1,69 @@
 #include <stdio.h>
-#include <string.h>   //strlen  
+#include <string.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <unistd.h>   //close  
-#include <arpa/inet.h>    //close  
+#include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros  
+#include <sys/time.h>
 
-#define TRUE   1
-#define FALSE  0
-#define PORT 8888
+#define PORT 2510
+#define STRING_SIZE 132
+
+// как работает сервер:
+// от пользователя прилетает строка длинной в 131 символ.
+// сначала проверя
+//
+//
+//0 2 0 2 0 2 0 2
+//2 0 2 0 2 0 2 0
+//0 2 0 2 0 2 0 2
+//0 0 0 0 0 0 0 0
+//0 0 0 0 0 0 0 0
+//0 1 0 1 0 1 0 1
+//1 0 1 0 1 0 1 0
+//0 1 0 1 0 1 0 1
+//
+//
+//
+
+char* update (char*);
 
 int main(int argc , char *argv[])
 {
-    int opt = TRUE;
+    int opt = 1;
     int master_socket , addrlen , new_socket , client_socket[30] ,
-            max_clients = 30 , activity, i , valread , sd;
+            max_clients = 2 , activity, i , valread , sd;
     int max_sd;
     struct sockaddr_in address;
 
-    char buffer[1025];  //data buffer of 1K  
-
-    //set of socket descriptors  
+    char buffer[STRING_SIZE];
     fd_set readfds;
 
-    //a message  
-    char *message = "ECHO Daemon v1.0 \r\n";
 
-    //initialise all client_socket[] to 0 so not checked  
     for (i = 0; i < max_clients; i++)
     {
         client_socket[i] = 0;
     }
 
-    //create a master socket  
     if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    //set master socket to allow multiple connections ,  
-    //this is just a good habit, it will work without this  
     if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )
     {
-        perror("setsockopt");
+        perror("setsockopt failed");
         exit(EXIT_FAILURE);
     }
 
-    //type of socket created  
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
 
-    //bind the socket to localhost port 8888  
     if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)
     {
         perror("bind failed");
@@ -63,43 +71,33 @@ int main(int argc , char *argv[])
     }
     printf("Listener on port %d \n", PORT);
 
-    //try to specify maximum of 3 pending connections for the master socket  
-    if (listen(master_socket, 3) < 0)
+    if (listen(master_socket, 2) < 0)
     {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    //accept the incoming connection  
     addrlen = sizeof(address);
     puts("Waiting for connections ...");
 
-    while(TRUE)
+    while(1)
     {
-        //clear the socket set  
         FD_ZERO(&readfds);
 
-        //add master socket to set  
         FD_SET(master_socket, &readfds);
         max_sd = master_socket;
 
-        //add child sockets to set  
         for ( i = 0 ; i < max_clients ; i++)
         {
-            //socket descriptor  
             sd = client_socket[i];
 
-            //if valid socket descriptor then add to read list  
             if(sd > 0)
                 FD_SET( sd , &readfds);
 
-            //highest file descriptor number, need it for the select function  
             if(sd > max_sd)
                 max_sd = sd;
         }
 
-        //wait for an activity on one of the sockets , timeout is NULL ,  
-        //so wait indefinitely  
         activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
 
         if ((activity < 0) && (errno!=EINTR))
@@ -107,8 +105,6 @@ int main(int argc , char *argv[])
             printf("select error");
         }
 
-        //If something happened on the master socket ,  
-        //then its an incoming connection  
         if (FD_ISSET(master_socket, &readfds))
         {
             if ((new_socket = accept(master_socket,
@@ -118,68 +114,115 @@ int main(int argc , char *argv[])
                 exit(EXIT_FAILURE);
             }
 
-            //inform user of socket number - used in send and receive commands  
             printf("New connection , socket fd is %d , ip is : %s , port : %d  \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs
                     (address.sin_port));
 
-            //send new connection greeting message  
-            if( send(new_socket, message, strlen(message), 0) != strlen(message) )
-            {
-                perror("send");
-            }
 
-            puts("Welcome message sent successfully");
 
-            //add new socket to array of sockets  
             for (i = 0; i < max_clients; i++)
             {
-                //if position is empty  
                 if( client_socket[i] == 0 )
                 {
                     client_socket[i] = new_socket;
                     printf("Adding to list of sockets as %d\n" , i);
 
+                    if (i == 0)
+                        send(client_socket[0], "0\n", 3, 0);
+                    if (i == 1)
+                    {
+                        send(client_socket[0], "1\n", 3, 0);
+                        send(client_socket[1], "2\n", 3, 0);
+                    }
                     break;
                 }
             }
         }
 
-        //else its some IO operation on some other socket 
         for (i = 0; i < max_clients; i++)
         {
             sd = client_socket[i];
 
             if (FD_ISSET( sd , &readfds))
             {
-                //Check if it was for closing , and also read the  
-                //incoming message  
-                if ((valread = read( sd , buffer, 1024)) == 0)
+                if ((valread =  read( sd , buffer, 1024)) == 0)
                 {
-                    //Somebody disconnected , get his details and print  
                     getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);
-                    printf("Host disconnected , ip %s , port %d \n" ,
-                           inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+                    printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
 
-                    //Close the socket and mark as 0 in list for reuse  
                     close( sd );
                     client_socket[i] = 0;
                 }
 
-                    //Echo back the message that came in
                 else
                 {
-                    //set the string terminating NULL byte on the end  
-                    //of the data read
                     if (sd == client_socket[0])
                         sd = client_socket[1];
                     else
                         sd = client_socket[0];
+
                     buffer[valread] = '\0';
-                    send(sd , buffer , strlen(buffer) , 0 );
+                    char* response = update(buffer);
+                    send(sd , response , strlen(buffer) , 0 );
+                    free(response);
                 }
             }
         }
     }
+}
 
-    return 0;
-}   
+
+
+char* update(char* req)
+{
+    int checkers[2];
+    char index = (char) strtol(&req[0], NULL, 10);
+
+        printf ("current index: %d\n", index);
+
+    checkers[0] = 0;
+    checkers[1] = 0;
+
+    for (int i = 2; req[i] != '\0'; i++)
+    {
+        if (index == '1')
+            switch (req[i])
+            {
+                case ('2'): checkers[0]++;
+                            break;
+
+                case ('5'): checkers[1]++;
+                            break;
+
+                default: ;
+            }
+        else
+            switch (req[i])
+            {
+                case ('1'): checkers[0]++;
+                        break;
+
+                case ('4'): checkers[1]++;
+                        break;
+
+                default: ;
+            }
+    }
+        printf("usual checkers: %d, big checkers: %d\n",checkers[0], checkers[1]);
+    if (checkers[0] == 0 && checkers[1] == 0)
+        index += 5;
+    else
+        index == 1 ? index++ : index--;
+
+    char* answer;
+    answer = (char*) malloc (sizeof(char) * STRING_SIZE);
+    answer[0] = (char) (index + '0');
+    answer[1] = ' ';
+    answer[2] = '\0';
+
+    strcpy(&answer[2], &req[2]);
+
+    puts (answer);
+
+    return answer;
+}
+
